8f128710f9e4c7d6795e182922298d1b3c104706
Joshua Crocker <joshua.crocker@hotmail.co.uk>
2017-06-16 14:46:44 +0100

    Started Problem 4

---
+++ Problem4/src/io/crocker/euler/problem4/Problem4.java
@@ -0,0 +1,22 @@
+package io.crocker.euler.problem4;
+
+public class Problem4 {
+	
+	/**
+	 * Problem 4: Largest Palindrome Product
+	 * 
+	 * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit
+	 * numbers is 9009 = 91 × 99.
+	 * 
+	 * Find the largest palindrome made from the product of two 3-digit numbers.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Problem4 problem4 = new Problem4();
+		
+		
+
+	}
+
+}
