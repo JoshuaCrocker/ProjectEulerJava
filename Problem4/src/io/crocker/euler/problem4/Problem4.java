d575e62738f9b1d605c2ccd2c5e3ac8bfa699ffb
Joshua Crocker <joshua.crocker@hotmail.co.uk>
2017-06-17 06:44:39 +0100

    Completed Problem 4

--- Problem4/src/io/crocker/euler/problem4/Problem4.java
+++ Problem4/src/io/crocker/euler/problem4/Problem4.java
@@ -15,8 +15,33 @@ public class Problem4 {
 	public static void main(String[] args) {
 		Problem4 problem4 = new Problem4();
 		
+		System.out.println("digits = 3: " + String.valueOf(problem4.LargestPalindromicNumber(3)));
+	}
+	
+	public int LargestPalindromicNumber(int digits) {
+		int min = (int) (1 * (Math.pow(10, digits - 1)));
+		int max = (int) (1 * (Math.pow(10, digits)));
 		
-
+		int largest = 0;
+		
+		for (int x = min; x < max; x++) {
+			for (int y = min; y < max; y++) {
+				int sum = x * y;
+				
+				if (this.IsPalindromic(sum) && sum > largest) {
+					largest = sum;
+				}
+			}
+		}
+		
+		return largest;
+	}
+	
+	private boolean IsPalindromic(int number) {
+		String numberStr = String.valueOf(number);
+		String reverse = new StringBuilder(numberStr).reverse().toString();
+		
+		return numberStr.equals(reverse);
 	}
 
 }
